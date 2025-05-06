a2996e702426b0cfa8979d5f0b6210cce8f2845a
Andrew Morris <voltrevo@gmail.com>
2025-05-06 14:10:12 +1000

    update server-server deps

--- server-server/src/utils/generateProtocol.ts
+++ server-server/src/utils/generateProtocol.ts
@@ -1,27 +1,16 @@
 import * as summon from 'summon-ts';
 import { readFileSync } from 'fs';
 import { Protocol } from 'mpc-framework';
-import { EmpWasmBackend } from 'emp-wasm-backend';
+import { EmpWasmEngine } from 'emp-wasm-engine';
 
 export default async function generateProtocol(mainFile: string) {
   await summon.init();
 
-  const { circuit } = summon.compileBoolean(mainFile, 16, (filePath: string) =>
-    readFileSync(filePath, 'utf8'),
-  );
+  const { circuit } = summon.compile({
+    path: mainFile,
+    boolifyWidth: 16,
+    readFile: filePath => readFileSync(filePath, 'utf8'),
+  });
 
-  const mpcSettings = [
-    {
-      name: 'alice',
-      inputs: ['a'],
-      outputs: ['main'],
-    },
-    {
-      name: 'bob',
-      inputs: ['b'],
-      outputs: ['main'],
-    },
-  ];
-
-  return new Protocol(circuit, mpcSettings, new EmpWasmBackend());
+  return new Protocol(circuit, new EmpWasmEngine());
 }
